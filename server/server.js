c1ce794463b0482a408bcd84c75757cb958d29e5
Sam Bassong <sam.bassong@gmail.com>
2024-02-02 14:50:29 -0800

    add star to send-file

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', AppRouter);
 
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
 });
 
